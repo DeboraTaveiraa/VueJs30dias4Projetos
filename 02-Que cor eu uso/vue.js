a7c7ce2637c5a54de6f22d969b43411caa1c534f
Débora Taveira <deborataveirapvh@gmail.com>
2021-01-25 19:46:46 -0400

    Add project 02 - Que cor eu uso?

---
+++ 02-Que cor eu uso/vue.js
@@ -0,0 +1,17 @@
+new Vue({
+    el: '#app',
+    data: {
+        cor1: '',
+        cor2: '',
+        cor3: '',
+        cor4: '',
+    },
+    methods: {
+        clear() {
+            this.cor1 = ''
+            this.cor2 = ''
+            this.cor3 = ''
+            this.cor4 = ''
+        }
+    }
+})
